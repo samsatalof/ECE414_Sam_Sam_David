d75b3376470062ad33d7479bc23a2a94cf4c0031
Samuel Owens <owenss@ad.lafayette.edu>
2024-09-04 15:33:31 -0400

    Lab 2 draft 1

---
+++ sw_in.c
@@ -0,0 +1,16 @@
+#include "sw_in.h"
+
+#define GPIO16_SW2 *(volatile uint32_t *) (0x40014080)
+#define GPIO17_SW1 *(volatile uint32_t *) (0x40014088)
+#define PADCTL_GPIO16 *(volatile uint32_t *) (0x4001c044)
+#define PADCTL_GPIO17 *(volatile uint32_t *) (0x4001c048)
+
+void sw_in_init() {
+    // GPIO 16/17 stat+ctrl is 0x40014000 offset 0x80/0x88
+    *PADCTL_GPIO16 = 0x00000044;
+    *PADCTL_GPIO17 = 0x00000044;
+}
+
+bool sw_in_read1() {
+
+}
