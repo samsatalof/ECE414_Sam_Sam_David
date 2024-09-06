4fdbfd7bc997c7abc13ec6cdaa903f6d71fa24e0
SamFOwens <owenss@lafayette.edu>
2024-09-06 09:50:16 -0400

    cleaned up defines

--- Lab2/sw_in.c
+++ Lab2/sw_in.c
@@ -1,20 +1,24 @@
 #include "sw_in.h"
 
-#define GPIO16_SW2 *(volatile uint32_t *) (0x40014080)
-#define GPIO17_SW1 *(volatile uint32_t *) (0x40014088)
-#define PADCTL_GPIO16 *(volatile uint32_t *) (0x4001c044)
-#define PADCTL_GPIO17 *(volatile uint32_t *) (0x4001c048)
+#define GPIO16_SW2 *(volatile uint32_t *) (IO_BANK0_BASE + IO_BANK0_GPIO17_STATUS_OFFSET)
+#define GPIO17_SW1 *(volatile uint32_t *) (IO_BANK0_BASE + IO_BANK0_GPIO16_STATUS_OFFSET)
+#define PADCTL_GPIO16 *(volatile uint32_t *) (PADS_BANK0_BASE + PADS_BANK0_GPIO16_OFFSET)
+#define PADCTL_GPIO17 *(volatile uint32_t *) (PADS_BANK0_BASE + PADS_BANK0_GPIO17_OFFSET)
 
 void sw_in_init() {
     // GPIO 16/17 stat+ctrl is 0x40014000 offset 0x80/0x88
-    *PADCTL_GPIO16 = 0x00000044;
-    *PADCTL_GPIO17 = 0x00000044;
+    PADCTL_GPIO16 = 0x00000044;
+    PADCTL_GPIO17 = 0x00000044;
 }
 
 bool sw_in_read1() {
-    digitalRead(17);
+    bool out;
+    out = digitalRead(17);
+    return out;
 }
 
 bool sw_in_read2() {
-    digitalRead(16);
+    bool out;
+    out = digitalRead(16);
+    return out;
 }
