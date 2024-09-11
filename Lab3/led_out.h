cc4ba735439f23c9ff28e3c389f54afa63cb7c95
samfowens <owenss@lafayette.edu>
2024-09-11 15:51:57 -0400

    Initial commit for Lab3

---
+++ Lab3/led_out.h
@@ -0,0 +1,12 @@
+#ifndef LED_OUT_H
+#define LED_OUT_H
+
+#include "pico/stdlib.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+void led_out_init();
+
+void led_out_write(uint8_t val);
+
+#endif
---
+++ Lab3/sw_in.h
@@ -0,0 +1,17 @@
+#ifndef SW_IN_H
+#define SW_IN_H
+
+#include "pico/stdlib.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+#define SW1_PIN 17
+#define SW2_PIN 16
+
+void sw_in_init();
+
+bool sw_in_read1();
+
+bool sw_in_read2();
+
+#endif
