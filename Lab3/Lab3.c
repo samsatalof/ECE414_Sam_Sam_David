2bb9d918d451e6b2c183d13ee4a7ff72b29f6709
samfowens <owenss@lafayette.edu>
2024-09-11 15:59:30 -0400

    add include statements

---
+++ Lab3/Lab3.c
@@ -0,0 +1,12 @@
+#include "pico/stdlib.h"
+#include "sw_in.h"
+#include <stdint.h>
+#include "led_out.h"
+#include <stdbool.h>
+#include "timer.h"
+#include "debounce_sw1.h"
+#include "debounce_sw2.h"
+
+int main() {
+    
+}
