3ac04b54228be07dbbe37f6232333a3959bc6d1e
SamFOwens <owenss@lafayette.edu>
2024-09-27 15:39:56 -0400

    Fixed code location, added rest of requirements

--- Lab4/Lab4.c
+++ Lab4/Lab4.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 uint16_t *px, *py;
-char buf [31];
+char buf[31];
 int main()
 {
     ts_lcd_init();
@@ -14,9 +14,12 @@ int main()
     {
         if (get_ts_lcd(px, py))
         {
-            tft_fillScreen(ILI9340_BLACK);
-            tft_drawLine(*px - 5, *py, *px + 5, *py, ILI9340_YELLOW);
-            tft_drawLine(*px, *py - 5, *px, *py + 5, ILI9340_YELLOW);
+            if ((px != NULL) && (py != NULL))
+            {
+                tft_fillScreen(ILI9340_BLACK);
+                tft_drawLine(*px - 5, *py, *px + 5, *py, ILI9340_YELLOW);
+                tft_drawLine(*px, *py - 5, *px, *py + 5, ILI9340_YELLOW);
+            }
         }
         else
         {
--- Lab4/ts_test.c
+++ Lab4/ts_test.c
@@ -1,5 +1,35 @@
 #include "ts_lcd.h"
+#include "TFTMaster.h"
+#include <inttypes.h>
+#include "TouchScreen.h"
+#include <stdbool.h>
+#include <stdio.h>
 
-void main() {
-    
+uint16_t *px, *py;
+char buf[31];
+int main()
+{
+    ts_lcd_init();
+    while (1)
+    {
+        if (get_ts_lcd(px, py))
+        {
+            if ((px != NULL) && (py != NULL))
+            {
+                tft_fillScreen(ILI9340_BLACK);
+                tft_drawLine(*px - 5, *py, *px + 5, *py, ILI9340_YELLOW);
+                tft_drawLine(*px, *py - 5, *px, *py + 5, ILI9340_YELLOW);
+                sprintf(buf, "Last location pressed was: %d, %d", *px, *py);
+                tft_writeString(buf);
+            }
+        }
+        else
+        {
+            tft_fillScreen(ILI9340_BLACK);
+            tft_drawLine(*px - 5, *py, *px + 5, *py, ILI9340_YELLOW);
+            tft_drawLine(*px, *py - 5, *px, *py + 5, ILI9340_YELLOW);
+            sprintf(buf, "Last location pressed was: %d, %d", *px, *py);
+            tft_writeString(buf);
+        }
+    }
 }
