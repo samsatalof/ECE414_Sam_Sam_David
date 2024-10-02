ed6c44f00fac2be3e110d681b92510bbe519a645
samfowens <owenss@lafayette.edu>
2024-10-02 10:29:52 -0400

    Final Push

--- Lab4/ts_test.c
+++ Lab4/ts_test.c
@@ -12,20 +12,23 @@ uint16_t x, y;
 
 int main()
 {
-
+    // Avoids malloc:
     px = &x;
     py = &y;
-    ts_lcd_init();
-    
 
-    while(1) {
+    ts_lcd_init(); // init function
+
+    while (1)
+    {
         if (get_ts_lcd(px, py))
         {
-            if ((px != NULL) && (py != NULL))
+            if ((px != NULL) && (py != NULL)) // Checks the pointers aren't NULL
             {
                 tft_fillScreen(ILI9340_BLACK);
+                // Crosshair:
                 tft_drawLine(*px - 5, *py, *px + 5, *py, ILI9340_YELLOW);
                 tft_drawLine(*px, *py - 5, *px, *py + 5, ILI9340_YELLOW);
+                // Location of press displayed as text:
                 sprintf(buf, "Last location pressed was: %d, %d", *px, *py);
                 tft_setCursor(10, 10);
                 tft_writeString(buf);
@@ -34,13 +37,14 @@ int main()
         else
         {
             tft_fillScreen(ILI9340_BLACK);
+            // Crosshair:
             tft_drawLine(*px - 5, *py, *px + 5, *py, ILI9340_YELLOW);
             tft_drawLine(*px, *py - 5, *px, *py + 5, ILI9340_YELLOW);
+            // Last location pressed displayed as text:
             sprintf(buf, "Last location pressed was: %d, %d", *px, *py);
             tft_setCursor(10, 10);
             tft_writeString(buf);
         }
-        sleep_ms(300);
-
+        sleep_ms(300); // Added to prevent excessive flickering
     }
 }
--- Lab4/unit_tests.c
+++ Lab4/unit_tests.c
@@ -17,7 +17,7 @@ int main()
     py = &y;
     ts_lcd_init();
 
-    // Unit Test TX
+    // Unit Test T3
     // while(1) {
     //     if (get_ts_lcd(px, py))
     //     {
@@ -30,5 +30,35 @@ int main()
     //     } else {
     //         tft_fillScreen(ILI9340_BLUE);
     //     }
-    // } 
+    // }
+
+    // Unit Test T4
+    // while (1)
+    // {
+    //     sprintf(buf, "Hello World!");
+    //     tft_setCursor(10, 10);
+    //     tft_writeString(buf);
+    // }
+
+    // Unit Test T5 was iteratively improved, and is now just ts_test
+    // Here's our earlier version that we used to calibrate the linear interpolation:
+    // while(1) {
+    //     if (get_ts_lcd(px, py))
+    //     {
+    //         if ((px != NULL) && (py != NULL))
+    //         {
+    //             tft_fillScreen(ILI9340_BLACK);
+    //             sprintf(buf, "Last location pressed was: %d, %d", *px, *py);
+    //             tft_setCursor(10, 10);
+    //             tft_writeString(buf);
+    //         }
+    //     }
+    //     else
+    //     {
+    //         tft_fillScreen(ILI9340_BLACK);
+    //         sprintf(buf, "Last location pressed was: %d, %d", *px, *py);
+    //         tft_setCursor(10, 10);
+    //         tft_writeString(buf);
+    //     }
+    // }
 }
