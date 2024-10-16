d26f44d2aefc5307f7be3dc2ef249302620cc611
samfowens <owenss@lafayette.edu>
2024-10-16 15:28:02 -0400

    FSM V1

--- Lab5/calc.c
+++ Lab5/calc.c
@@ -14,5 +14,119 @@ David Green, Sam Satalof, Sam Owens */
 
 int main()
 {
-    enum
+    // FSM States:
+    enum CALC_States
+    {
+        Operand,
+        Operator,
+        Operand_2,
+        Result,
+        DIV_0,
+        Error,
+        CLR
+
+    } CALC_State;
+
+    void tick()
+    {
+
+        switch (CALC_State)
+        {
+        case Operand:
+            if (operand_overwrite = true)
+            {
+                CALC_State = Operand;
+            }
+            if (clr_pressed = true)
+            {
+                CALC_State = CLR;
+            }
+            if (new_operator() = true)
+            {
+                CALC_State = Operator
+            }
+            
+
+            break;
+        case Operator:
+            if (clr_pressed = true)
+            {
+                CALC_State = CLR;
+            }
+            if (operator_overwrite() = true)
+            {
+                CALC_State = Operator;
+            }
+            if (new_operand() = true)
+            {
+                CALC_State = Operand_2;
+            }
+            
+            
+
+            break;
+        case Operand_2:
+            if (clr_pressed = true)
+            {
+                CALC_State = CLR;
+            }
+            if (operand2_overwrite() = true)
+            {
+                CALC_State = Operand_2;
+            }
+            if (div_0_check() = true)
+            {
+                CALC_State = DIV_0;
+            }
+            if (overflow_check() = true)
+            {
+                CALC_State = Error;
+            }
+            if (eqls_pressed() = true)
+            {
+                CALC_State = Result;
+            }
+            
+            break;
+        case Result:
+            if (clr_pressed = true)
+            {
+                CALC_State = CLR;
+            }
+            break;
+            if (clr_pressed() = false)
+            {
+                CALC_State = Result;
+            }
+            
+
+        case DIV_0:
+            if (clr_pressed = true)
+            {
+                CALC_State = CLR;
+            }
+            if (clr_pressed() = false)
+            {
+                CALC_State = DIV_0;
+            }
+            break;
+        case CLR:
+            if (clr_pressed = true)
+            {
+                CALC_State = CLR;
+            }
+            if (clr_pressed() = false)
+            {
+                CALC_State = Error;
+            }
+            if (boot_to_start() = true)
+            {
+                CALC_State = Operand;
+            }
+            
+            break;
+        default:
+            break;
+        }
+    }
 }
