ed1dc9c7c592425544aa6e336ddc3b0760826244
David Green <davidgreen@Davids-M1-MacBook-Pro.local>
2024-10-16 15:00:54 -0400

    stuff

---
+++ Lab5/calc.c
@@ -0,0 +1,18 @@
+/* Main program for Lab05 Calculator
+This program contains the state machine that performs the arithmetical functions of the calculator.
+David Green, Sam Satalof, Sam Owens */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "pico/stdlib.h"
+#include "ts_lcd.h"
+#include "TFTMaster.h"
+#include "TouchScreen.h"
+#include "hardware/adc.h"
+
+int main()
+{
+    enum
+}
