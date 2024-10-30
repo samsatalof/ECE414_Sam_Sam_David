f06d32ad6bf0b20e333af5b63084402bc16d24d1
samfowens <owenss@lafayette.edu>
2024-10-30 14:59:21 -0400

    Added dependencies

---
+++ Lab6/ts_lcd.c
@@ -0,0 +1,35 @@
+#include "ts_lcd.h"
+#include "pico/stdlib.h"
+#include "TFTMaster.h"
+#include "TouchScreen.h"
+#include "hardware/adc.h"
+
+bool get_ts_lcd(uint16_t *px, uint16_t *py)
+{
+
+    struct TSPoint p;
+    getPoint(&p);
+    if (!(p.z < 8000))
+    {
+        return false;
+    }
+    else
+    {
+        *px = (p.y * (ILI9340_TFTHEIGHT)) / (touchscrn_height);
+        *py = (p.x * (ILI9340_TFTWIDTH)) / (touchscrn_width);
+        return true;
+    }
+}
+
+void ts_lcd_init()
+{
+    stdio_init_all();
+    adc_init();
+    tft_init_hw();
+    tft_begin();
+    tft_setRotation(1);
+    tft_fillScreen(ILI9340_BLACK);
+    tft_setTextSize(2);
+    tft_setTextColor2(ILI9340_MAGENTA,ILI9340_BLACK);
+    tft_setCursor(10,10);
+}
---
+++ Lab6/ts_lcd.h
@@ -0,0 +1,32 @@
+/*
+    Touchscreen driver that translates touchscreen raw data into LCD screen coordinates
+
+    Authors: Sam Owens, David Green, Sam Satalof
+*/
+
+#ifndef TS_LCD_H
+#define TS_LCD_H
+
+#include <stdbool.h>
+#include <inttypes.h>
+
+#define CLK_GPIO 17
+#define MOSI_GPIO 19
+#define CS_GPIO 18
+#define DC_GPIO 16
+#define RST_GPIO 20
+#define Xpos_GPIO 22
+#define Xneg_GPIO 26
+#define Ypos_GPIO 27
+#define Yneg_GPIO 21
+#define touchscrn_width 4000
+#define touchscrn_height 3800
+
+// Reads the status of touchscreen, returning true when a finger/stylus is placed on display,
+// Two pointers are then assigned current finger/stylus position in LCD coordinates
+bool get_ts_lcd(uint16_t *px, uint16_t *py);
+
+// Initializes and configures everything necessary to read touchscreen
+void ts_lcd_init();
+
+#endif
