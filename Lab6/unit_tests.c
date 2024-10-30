bd3d983aa706b4c363fbb22d72d082e5f6d21124
David Green <davidigreen56@gmail.com>
2024-10-30 14:55:56 -0400

    started first unit test

---
+++ Lab6/unit_tests.c
@@ -0,0 +1,8 @@
+#define UART_ID uart0
+#define BAUD_RATE 115200
+// We are using pins 0 and 1, but see the GPIO function select table in the
+// datasheet for information on which other pins can be used.
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+#include <stdio.h>
